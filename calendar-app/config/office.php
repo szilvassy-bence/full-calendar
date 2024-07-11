87324c143630f478fca71fb271a53e9e6f190368
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-11 18:04:12 +0200

    updated office.php to get the closed days also

--- calendar-app/config/office.php
+++ calendar-app/config/office.php
@@ -3,4 +3,8 @@
 return [
     'opening_hour' => 8, // Example opening hour
     'closing_hour' => 20, // Example closing hour
+    'closed_days' => [
+        \App\Enums\Day::SATURDAY,
+        \App\Enums\Day::SUNDAY
+    ]
 ];
